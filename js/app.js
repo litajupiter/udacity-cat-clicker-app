1cb684fa2fe5cb4f2ac76f31260ebdce249df2ca
Brianne <brianne.hennel@gmail.com>
2018-07-14 17:43:21 -0400

    Added cat's names to the counter box

--- js/app.js
+++ js/app.js
@@ -1,13 +1,8 @@
 var firstCat = document.getElementById('catPic');
-
 var secondCat = document.getElementById('catPic2');
-
 var clickCounterOne = 0;
-
 var clickCounterTwo = 0;
-
 var firstCatClicks = document.getElementById('numClicks');
-
 var secondCatClicks = document.getElementById('numClicks2');
 
 firstCat.addEventListener('click', function() {
