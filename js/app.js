bd0fd36e5a19ed7857eb9d78fdc0f19aa3227318
Brianne <brianne.hennel@gmail.com>
2018-08-02 20:07:58 -0400

    Added counters for each cat

--- js/app.js
+++ js/app.js
@@ -12,33 +12,45 @@ const catpic3 = document.getElementById("catpic3");
 const catpic4 = document.getElementById("catpic4");
 const catpic5 = document.getElementById("catpic5");
 
-const catcounter1 = document.getElementById("catCounter1");
+const counter1 = document.getElementById("counter1");
+const counter2 = document.getElementById("counter2");
+const counter3 = document.getElementById("counter3");
+const counter4 = document.getElementById("counter4");
+const counter5 = document.getElementById("counter5");
 
 catname1.addEventListener('click', function() {
-   display.innerHTML = "<img src=\"img/catface1.jpg\">";
+  counter1.style.display = "block";
+  catpic1.style.display = "block";
 }, false);
 
 catname2.addEventListener('click', function() {
-   display.innerHTML = "<img src=\"img/catleopard.jpg\">";
+  catpic1.style.display = "none";
+  catpic2.style.display = "block";
 }, false);
 
-catname3.addEventListener('click', function() {
-   display.innerHTML = "<img src=\"img/catforward.jpg\">";
-}, false);
-
-catname4.addEventListener('click', function() {
-   display.innerHTML = "<img src=\"img/catup.jpg\">";
-}, false);
-
-catname5.addEventListener('click', function() {
-   display.innerHTML = "<img src=\"img/catcoverface.jpg\">";
-}, false);
-
-
+//write a function to add and remove the show/hidden classes and add to the eventlistener?
+//will the click counter work this way?
 
 // catname1.addEventListener('click', function() {
-//    catpic1.style.display = "block";
+//    display.innerHTML = "<img src=\"img/catface1.jpg\">";
 // }, false);
+//
+// catname2.addEventListener('click', function() {
+//    display.innerHTML = "<img src=\"img/catleopard.jpg\">";
+// }, false);
+//
+// catname3.addEventListener('click', function() {
+//    display.innerHTML = "<img src=\"img/catforward.jpg\">";
+// }, false);
+//
+// catname4.addEventListener('click', function() {
+//    display.innerHTML = "<img src=\"img/catup.jpg\">";
+// }, false);
+//
+// catname5.addEventListener('click', function() {
+//    display.innerHTML = "<img src=\"img/catcoverface.jpg\">";
+// }, false);
+
 
 
 // var firstCat = document.getElementById('catPic');
@@ -57,3 +69,8 @@ catname5.addEventListener('click', function() {
 //   clickCounterTwo += 1;
 //   secondCatClicks.innerHTML = `${clickCounterTwo}`;
 // }, false);
+
+
+// use the classList API to remove and add classes
+//div.classList.remove("foo");
+//div.classList.add("anotherclass");
